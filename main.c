165c7cbb89fc7d655625de1e0690d7d1e82d1937
artisticmclaren <elijahjvandeventer@gmail.com>
2024-07-07 16:21:52 -0400

    WINDOW class

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <ncurses.h>
 
+bool running = true;
+
 int main(int argc, char ** argv) {
+	int x,y;
 	initscr();
+	getmaxyx(stdscr,y,x);
+	WINDOW * win = newwin(y,x,0,0);
 	refresh();
-	int ch = getch();
-	printw("%d",ch);
-	getch();
+	wrefresh(win);
+	while (running==true) {
+		box(win,0,0);
+		wprintw(win,"Hello World!");
+		refresh();
+		wrefresh(win);
+		int ch = getchar();
+		if (ch==113) { // q: quit
+			running=false;
+		}
+	}
 	endwin();
 	return 0;
 }
