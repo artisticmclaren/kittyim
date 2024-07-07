8f93a1ba244a151b3c340b19d59a314da301482c
artisticmclaren <elijahjvandeventer@gmail.com>
2024-07-07 15:56:01 -0400

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# kittyim
+a terminal based image viewer for any terminal with the kitty image protocol (konsole,wezterm,wayst).
+## Installation / Build from source
+download the repository or git clone it with ```git clone https://github.com/artisticmclaren/kittyim.git```. cd into kittyim and run ```sudo bash compile.sh``` and then ```kittyim``` to start.
+
+Enjoy!
--- compile.sh
+++ compile.sh
@@ -1,17 +1,11 @@
-if gcc -o kittyim main.c -lncurses; then
+if [ "$EUID" -ne 0 ]
+then
+	echo "Please run with root permissions."
+	exit
+fi
+
+if gcc -o /bin/kittyim main.c -lncurses; then
 	echo Compiled successfully.
 else
 	echo Compilation failed.
 fi
-
-if [ "$EUID" -ne 0 ]
-then
-	echo "Cannot move to /bin, executing local file."
-	./kittyim
-	exit
-fi
-
-echo "Moving to /bin"
-mv kittyim /bin/kittyim
-echo "Done."
-exit
